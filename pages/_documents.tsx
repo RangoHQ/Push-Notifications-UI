36bb087db600d778eed8f4db61f2c18f253cc3c1
oliverloops <oliveralr@protonmail.com>
2022-04-26 12:31:19 -0500

    Initial commit

---
+++ pages/_documents.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<any> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: <>{initialProps.styles}</>,
+    };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
