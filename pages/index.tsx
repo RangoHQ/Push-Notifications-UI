b2591d25c4ef6a79a6e68103e29638b6e5068bf8
oliverloops <oliveralr@protonmail.com>
2022-04-26 12:37:10 -0500

    🎨 Styles updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import { Button } from "@nextui-org/react";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Push Notitifications Client</h1>
       <Button>Click Me</Button>
     </div>
