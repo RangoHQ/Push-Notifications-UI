3c05ea7998db8c373e5e937e98c8618a89bc3e0a
oliverloops <oliveralr@protonmail.com>
2022-04-26 23:56:23 -0500

    🐦 Push notifications server connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import {
   Container,
@@ -8,16 +9,42 @@ import {
   Input,
   Text,
   Textarea,
+  Loading,
 } from "@nextui-org/react";
 
 //Layout
 import Footer from "../layout/Footer";
-
 //Utils
 import { useMediaQuery } from "../utils/useMediaQuery";
 
+const NOTIFICATIONS_SERVER: string =
+  "https://kluu-push-notifications.herokuapp.com/message";
+
 const Home: NextPage = () => {
   const isMd = useMediaQuery(768);
+  const [fields, setFields] = useState({ title: "", body: "" });
+  const [loading, setLoading] = useState(false);
+
+  const handleFields = (e: any) =>
+    setFields({ ...fields, [e.target.name]: e.target.value });
+
+  const sendNotification = async (e: any) => {
+    e.preventDefault();
+    const title = fields.title;
+    const body = fields.body;
+    if (title === "" || body === "") {
+      alert("title/body cannot be empty");
+      return;
+    }
+
+    setLoading(true);
+    const req = await fetch(NOTIFICATIONS_SERVER, {
+      method: "POST",
+      body: JSON.stringify({ title, body }),
+    });
+    setFields({ title: "", body: "" });
+    setLoading(false);
+  };
 
   return (
     <Container>
@@ -37,26 +64,34 @@ const Home: NextPage = () => {
         </Text>
         <Spacer y={2.5} />
         <Container css={{ width: isMd ? "100%" : "624px" }}>
-          <form>
+          <form onSubmit={sendNotification} id="PNform">
             <Grid.Container justify="center" direction="column">
               <Input
+                required
                 clearable
                 bordered
                 label="Titulo"
                 labelPlaceholder="Title"
                 color="primary"
+                onChange={handleFields}
               />
               <Spacer y={2} />
               <Textarea
+                required
                 bordered
                 labelPlaceholder="Descripcion"
                 color="primary"
+                onChange={handleFields}
               />
             </Grid.Container>
             <Spacer y={2.5} />
             <Row justify="center">
-              <Button shadow size="lg" color="gradient" rounded>
-                Enviar
+              <Button type="submit" shadow size="lg" color="gradient" rounded>
+                {loading ? (
+                  <Loading type="points" color="currentColor" size="sm" />
+                ) : (
+                  <Text> Enviar Notificación</Text>
+                )}
               </Button>
             </Row>
           </form>
